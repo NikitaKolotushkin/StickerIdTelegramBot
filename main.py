789163b03e30092ed0cfa51b67601f806b00a6a9
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-09-13 13:31:20 +0400

    PEP8

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ import telebot
 
 bot = telebot.TeleBot("your_token")
 
+
 @bot.message_handler(commands=["start"])
 def starting_message(message):
     bot.send_message(message.chat.id, "Hello! To find out the sticker ID, send me any sticker")
+
 
 @bot.message_handler(content_types=["sticker"])
 def get_sticker_id(message):
     bot.send_message(message.chat.id, message.sticker.file_id)
 
+
 @bot.message_handler(content_types=["text"])
 def bot_sending_sticker(message):
     bot.send_sticker(message.chat.id, message.text)
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
