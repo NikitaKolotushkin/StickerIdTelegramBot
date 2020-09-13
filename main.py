38ddf9ecd84b9a36e5989458e9b9b88f3af42768
NikitaKolotushkin <nikitaplay1000@gmail.com>
2020-09-13 12:52:54 +0400

    Added basic functionality

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ bot = telebot.TeleBot(tocken)
 
 @bot.message_handler(commands=["start"])
 def starting_message(message):
-    bot.send_message(message.chat.id, "Hi, you write /start command!")
+    bot.send_message(message.chat.id, "Hello! To find out the sticker ID, send me any sticker")
+
+@bot.message_handler(content_types=["sticker"])
+def get_sticker_id(message):
+    bot.send_message(message.chat.id, message.sticker.file_id)
+
+@bot.message_handler(content_types=["text"])
+def bot_sending_sticker(message):
+    bot.send_sticker(message.chat.id, message.text)
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
